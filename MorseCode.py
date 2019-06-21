5c66191f06bac1b21c5fb5f2eab8e63cc7e79045
HuitingZhengAvery <avery.zheng.ht@gmail.com>
2019-06-21 23:44:24 +1000

    Add files via upload

---
+++ MorseCode.py
@@ -0,0 +1,25 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        fin = []
+        morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        alphabet = []
+        for letter in range(97,123):
+            alphabet.append(chr(letter))
+        ref = dict(zip(alphabet, morse))
+
+        if not words:
+            return 0
+        if words:
+            sml = str()
+            lrg = []
+            for i in range(len(words)):
+                cur = words[i]
+                for j in range(len(cur)):
+                    n = ref[cur[j]]
+                    sml += n
+                lrg.append(sml)
+                sml = str()
+        for t in lrg:
+            if t not in fin:
+                fin.append(t)
+        return len(fin)
